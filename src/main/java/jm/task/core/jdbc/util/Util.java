28f8fd24799a754efa402829129c6c9b4937af98
span4 <span464@gmail.com>
2026-04-03 03:27:04 +0900

    Class Util is ready for check, master Mentornim! Omg its so cool that i can type here whatever i want and wont get punished for this :D

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // set up a database connection
+    protected static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/habsida_task_1";
+        String user = "root";
+        String password = "1234";
+
+        try {
+            Connection connection =
+                    DriverManager.getConnection(url, user, password);
+            System.out.println("Connection ON");
+            return connection;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
